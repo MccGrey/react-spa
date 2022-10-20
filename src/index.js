f37855ab0aec375a69252f69ba5b78bf5410d41c
MccGrey <chuxzzie01@yahoo.com>
2022-10-20 06:22:38 +0100

    create components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import Main from "./Main";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+ReactDOM.render(<Main />, document.getElementById("root"));
